b0baea277ff8fcaaa4b6e4ae5d7efd8af21d68ac
William Wong <compulim@hotmail.com>
2018-03-15 17:46:54 -0700

    Add version-commit script

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,18 @@ const LastCommitLog = require('last-commit-log');
 const packageJSON = require('./package.json');
 const semver = require('semver');
 
-gulp.task('commit-publish', async () => {
+gulp.task('version-commit', async () => {
   console.log(process.env);
 
-  const { TRAVIS_BRANCH, TRAVIS_PULL_REQUEST } = process.env;
-
-  if (TRAVIS_BRANCH === 'master' && TRAVIS_PULL_REQUEST === 'false') {
-    console.log('should NPM publish');
-  }
-
+  const { TRAVIS_BRANCH } = process.env;
   const lastCommitLog = new LastCommitLog(__dirname);
   const log = await lastCommitLog.getLastCommit();
   const { version } = packageJSON;
-  const nextVersion = `${ semver.major(version) }.${ semver.minor(version) }.${ semver.patch(version) }-master.${ log.shortHash }`;
+  const nextVersion = `${ semver.major(version) }.${ semver.minor(version) }.${ semver.patch(version) }-${ TRAVIS_BRANCH }.${ log.shortHash }`;
 
   console.log(`version: ${ version } -> ${ nextVersion }`);
 
   return gulp
     .src('package.json')
-    .pipe(exec(`npm --allow-same-version --no-commit-hooks --no-git-tag-version version ${ nextVersion }`))
-    // .pipe(exec('npm publish --tag=master'));
+    .pipe(exec(`npm --allow-same-version --no-commit-hooks --no-git-tag-version version ${ nextVersion }`));
 });
