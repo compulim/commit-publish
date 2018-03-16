100f80d260b55ec812a9d6560d7faea251c6a7ad
William Wong <compulim@hotmail.com>
2018-03-15 19:08:51 -0700

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 const exec = require('gulp-exec');
 const gulp = require('gulp');
 const LastCommitLog = require('last-commit-log');
+const { major, minor, patch } = require('semver');
+
 const packageJSON = require('./package.json');
-const semver = require('semver');
 
 gulp.task('version-commit', async () => {
-  console.log(process.env);
-
-  const { TRAVIS_BRANCH } = process.env;
   const lastCommitLog = new LastCommitLog(__dirname);
   const log = await lastCommitLog.getLastCommit();
   const { version } = packageJSON;
-  const nextVersion = `${ semver.major(version) }.${ semver.minor(version) }.${ semver.patch(version) }-${ TRAVIS_BRANCH }.${ log.shortHash }`;
+  const nextVersion = `${ major(version) }.${ minor(version) }.${ patch(version) }-${ process.env.TRAVIS_BRANCH }.${ log.shortHash }`;
 
-  console.log(`version: ${ version } -> ${ nextVersion }`);
+  console.log(`Version: ${ version } -> ${ nextVersion }`);
 
   return gulp
     .src('package.json')
