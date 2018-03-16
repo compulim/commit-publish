539f41541ec6b37e1e36b84c3200452069775c29
William Wong <compulim@hotmail.com>
2018-03-15 20:23:22 -0700

    Add GitHub Releases deployment

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,22 @@ const { major, minor, patch } = require('semver');
 const packageJSON = require('./package.json');
 
 gulp.task('version-commit', async () => {
+  const { TRAVIS_BRANCH } = process.env;
   const lastCommitLog = new LastCommitLog(__dirname);
   const log = await lastCommitLog.getLastCommit();
   const { version } = packageJSON;
-  const nextVersion = `${ major(version) }.${ minor(version) }.${ patch(version) }-${ process.env.TRAVIS_BRANCH }.${ log.shortHash }`;
 
-  console.log(`Version: ${ version } -> ${ nextVersion }`);
+  let nextVersion = `${ major(version) }.${ minor(version) }.${ patch(version) }`;
+
+  if (TRAVIS_BRANCH === 'release') {
+    console.log(`Release version: ${ version } -> ${ nextVersion }`);
+  } else {
+    nextVersion += `-${ process.env.TRAVIS_BRANCH }.${ log.shortHash }`;
+
+    console.log(`Pre-release version: ${ version } -> ${ nextVersion }`);
+  }
 
   return gulp
     .src('package.json')
-    .pipe(exec(`npm --allow-same-version --no-commit-hooks --no-git-tag-version version ${ nextVersion }`));
+    .pipe(exec(`npm --no-git-tag-version version ${ nextVersion }`));
 });
